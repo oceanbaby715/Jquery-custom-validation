8309497671b188c4ff863c1ed31cb182fff25656
Terry <liuhaiyang715@163.com>
2017-08-16 10:54:33 +0800

    新批电信199（0-9）号段、移动198（0-9）号段、联通166（0-9）号段

--- dist/jquery.custom.validation.js
+++ dist/jquery.custom.validation.js
@@ -364,7 +364,7 @@ jQuery.validator.addMethod("isMediumPassword", function(value, element) {
  */
 jQuery.validator.addMethod("isMobile", function(value, element) {
 	var length = value.length;
-	var reg = /^(((13[0-9]{1})|(14[5|7]{1})|(15[0|1|2|3|5|6|7|8|9]{1})|(17[0|3|6|7|8]{1})|(18[0-9]{1}))+\d{8})$/;
+	var reg = /^(((13[0-9]{1})|(14[57]{1})|(15[012356789]{1})|(17[03678]{1})|(18[0-9]{1})|(19[89]{1})|(16[6]{1}))+\d{8})$/;
 	return this.optional(element) || (length == 11 && reg.test(value));
 }, "请输入正确的手机号码");;
 /*！
